c79b0af5d83542f96e2d5a49b6210aa9654cfd51
Senuji De Silva <senujihasara2003@gmail.com>
2025-03-23 11:32:36 +0530

    checking bugs

--- .next/static/chunks/app_map_page_tsx_b741fd61._.js
+++ .next/static/chunks/app_map_page_tsx_b741fd61._.js
@@ -5,7 +5,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/app_map_page_tsx_b741fd61._.js",
   "chunks": [
-    "static/chunks/[root of the server]__0436e0d7._.js",
+    "static/chunks/[root of the server]__d4b54a3a._.js",
     "static/chunks/app_696081d0._.js",
     "static/chunks/node_modules_next_00b8f9a3._.js",
     "static/chunks/node_modules_framer-motion_dist_es_059f4bd4._.js",
